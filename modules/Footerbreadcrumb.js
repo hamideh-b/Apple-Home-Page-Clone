cf59d60620c1590742a05bfb8ede4d889a3ae625
Hamideh.B <hamideh.b120@gmail.com>
2025-07-27 23:16:28 +0430

    breadcrumb edited

--- modules/Footerbreadcrumb.js
+++ modules/Footerbreadcrumb.js
@@ -3,8 +3,8 @@ const Footerbreadcrumb = async () => {
   let response = await data.json();
   let footer = response.footerbreadcrumb.map((elem) => {
     return `
-            <li class="breadcrumb-item">
-                <a href="" class="nav-link d-inline-block">
+            <li class="breadcrumb-item p-0 pe-1">
+                <a href="" class="nav-link d-inline-block ">
                 ${elem.name}
                 </a>
             </li>
